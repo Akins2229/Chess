f6ff2dee6b4b2f5bc1758bbb1a0c0441bb6a304f
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-08 18:00:34 -0700

    Create Bishop.py

---
+++ pieces/Bishop.py
@@ -0,0 +1,18 @@
+import typing
+
+from colors import White
+from colors import Black
+
+class Bishop:
+    def __init__(
+        self,
+        color: typing.Union(White, Black),
+        pos: typing.Tuple[int, int]
+    ) -> None:
+        self.color=color
+        self.rank=pos[0]
+        self.file=pos[1]
+        self.pos=pos
+
+    def __str__(self) -> str:
+        return "Bishop"
