d5d1d07ba433edb7acace9f52f6b81720e4fef4c
Akins <82357502+Akins2229@users.noreply.github.com>
2021-09-08 18:04:21 -0700

    Create colors.py

---
+++ colors.py
@@ -0,0 +1,7 @@
+class White:
+    def __str__(self) -> int:
+        return 0xffffff #return hex for white
+
+class Black:
+    def __str__(self) -> int:
+        return 0x000000 #return hex for black
